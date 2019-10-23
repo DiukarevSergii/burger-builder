339abc4e24438e902bd8fccccee491b77b5bd5dc
Diukarev <diukarev.sergii@gmail.com>
2019-10-23 10:21:22 +0300

    outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => (
-  <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="bread-bottom" />
-  </div>
-);
+const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+  // eslint-disable-next-line react/no-array-index-key
+    .map((key) => [...Array(props.ingredients[key])].map((_, i) => <BurgerIngredient key={key + i} type={key} />));
+
+  return (
+
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+Burger.propTypes = {
+  ingredients: PropTypes.object.isRequired,
+};
+
 
 export default Burger;
