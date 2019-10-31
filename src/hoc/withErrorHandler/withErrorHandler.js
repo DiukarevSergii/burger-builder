dcf0b10a25a2343555b0a79a05e254bfb9c29167
Diukarev <diukarev.sergii@gmail.com>
2019-10-31 12:05:59 +0200

    error handling with hoc

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import Aux from '../Aux/Aux';
+import Modal from '../../components/UI/Modal/Modal';
+
+
+const withErrorHandler = (WrappedComponent, axios) => class extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+    };
+  }
+
+  componentWillMount() {
+    axios.interceptors.request.use((req) => {
+      this.setState({ error: null });
+      return req;
+    });
+    axios.interceptors.response.use((res) => res, (error) => {
+      this.setState({ error });
+    });
+  }
+
+  errorConfirmedHandler = () => {
+    this.setState({ error: null });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Modal
+          show={this.state.error}
+          modalClosed={this.errorConfirmedHandler}
+        >
+          {this.state.error ? this.state.error.message : null}
+        </Modal>
+        <WrappedComponent />
+      </Aux>
+    );
+  }
+};
+
+export default withErrorHandler;
