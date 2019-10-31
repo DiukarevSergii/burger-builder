3d78c290d198973a9f6d5d785ceb8921d2d23844
Diukarev <diukarev.sergii@gmail.com>
2019-10-31 10:05:16 +0200

    add axios for http request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -80,8 +81,29 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // eslint-disable-next-line no-alert, no-undef
-    alert('You continue!');
+    // alert('You continue!');
+    // this.setState({ loading: true });
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Diukarev Serhii',
+        address: {
+          street: 'Palo Alto, San Francisco Bay Area',
+          country: 'USA',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+    axios.post('/orders.json', order)
+      .then((response) => {
+        console.log(response);
+        // this.setState({ loading: false, purchasing: false });
+      });
+    // .catch((error) => {
+    //   this.setState({ loading: false, purchasing: false });
+    // });
   };
 
   updatePurchaseState(ingredients) {
